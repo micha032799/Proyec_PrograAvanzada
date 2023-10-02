8b68f4c38d366c40dee3b578e8ae53e94c1b592d
Diegoespiher <diegoespinach12@gmail.com>
2023-10-01 18:43:27 -0600

    Clase 3

---
+++ Proyecto_PA/Proyecto_PA/Entities/UsuarioEnt.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Proyecto_PA.Entities
+{
+    public class UsuarioEnt
+    {
+        public String Identificacion {  get; set; }
+        public String Nombre { get; set;}
+        public String Correo { get; set; }
+        public String Contrasenna { get; set; }
+    }
+}
