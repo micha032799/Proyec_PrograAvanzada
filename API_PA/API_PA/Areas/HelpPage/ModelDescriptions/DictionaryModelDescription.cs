2616fdc7219b57c4db9f71a019f8469a145c1e93
Diegoespiher <diegoespinach12@gmail.com>
2023-10-01 19:00:54 -0600

    Clase 3 API

---
+++ API_PA/API_PA/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace API_PA.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ API_PA/API_PA/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="API_PA.WebApiApplication" Language="C#" %>
