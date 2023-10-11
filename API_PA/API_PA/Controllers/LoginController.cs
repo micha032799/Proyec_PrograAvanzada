e5fb8f902d670bde73475cac5c4db082aca9fb12
proisgod <94262673+proisgod@users.noreply.github.com>
2023-10-10 22:37:51 -0600

    Clase 4, BD y API
    Texto de la creación de la base de datos para Microsoft SQL SMS, agregado de la clase #4, y cambios de version de libreria.

---
+++ API_PA/API_PA/Controllers/LoginController.cs
@@ -0,0 +1,48 @@
+﻿using API_PA.Entities;
+using System.Linq;
+using System.Web.Http;
+
+namespace API_PA.Controllers
+{
+    public class LoginController : ApiController
+    {
+        [HttpPost]
+        [Route("RegistrarCuenta")]
+        public string RegistrarCuenta(UsuarioEnt entidad)
+        {
+            using (var context = new PAEntities())
+            {
+                //TUsuario user = new TUsuario();
+                //user.Identificacion = entidad.Identificacion;
+                //user.Nombre = entidad.Nombre;
+                //user.Correo = entidad.Correo;
+                //user.Contrasena = entidad.Contrasena;
+                //user.Estado = entidad.Estado;
+                //user.Direccion = entidad.Direccion;
+
+                //context.TUsuario.Add(user);
+                //context.SaveChanges();
+
+                context.RegistrarCuentaSP(entidad.Identificacion, entidad.Nombre, entidad.Correo, entidad.Contrasena, entidad.Estado, entidad.Direccion);
+
+                return "Registro realizado exitosamente!";
+            }
+        }
+        [HttpPost]
+        [Route("IniciarSesion")]
+        public IniciarSesionSP_Result IniciarSesion(UsuarioEnt entidad)
+        {
+            using (var context = new PAEntities())
+            {
+                //var datos = (from x in context.TUsuario
+                //             where x.Correo == entidad.Correo
+                //             && x.Contrasena == entidad.Contrasena
+                //             && x.Estado == true
+                //             select x).FirstOrDefault();
+
+                return context.IniciarSesionSP(entidad.Correo, entidad.Contrasena).FirstOrDefault();
+
+            }
+        }
+    }
+}
--- API_PA/API_PA/Entities/UsuarioEnt.cs
+++ API_PA/API_PA/Entities/UsuarioEnt.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace Proyecto_PA.Entities
+﻿namespace API_PA.Entities
 {
     public class UsuarioEnt
     {
